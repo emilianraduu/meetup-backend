9f34b3876b8b987f91d7f64a89b2c3d9c28a63dd
Emilian Radu <emiradu98@icloud.com>
2021-06-17 22:04:33 +0300

    fixed any type

--- src/resolvers/Queries/User.ts
+++ src/resolvers/Queries/User.ts
@@ -1,7 +1,8 @@
 import { list, nonNull, queryField, stringArg } from 'nexus'
 import { handleError } from '../../utils/helpers'
 import { errors } from '../../utils/constants'
-import { User } from '../models'
+import { User } from '.prisma/client'
+
 
 export const me = queryField('me', {
   type: 'User',
@@ -67,10 +68,9 @@ export const findUsers = queryField('findUsers', {
   async resolve(_parent, { email }, ctx) {
     try {
       const users = await ctx.prisma.user.findMany()
-      const filteredUsers = users.filter(user => {
+      return users.filter((user: User) => {
         return user.email.startsWith(email)
       })
-      return filteredUsers
     } catch (e) {
       console.log(e)
       handleError(errors.userAlreadyExists)
