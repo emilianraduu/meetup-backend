3cf64a0a9388a48d3e732b55cf6b2bd57003496e
Emilian Radu <emiradu98@icloud.com>
2021-06-17 21:37:44 +0300

    123

--- src/resolvers/Queries/User.ts
+++ src/resolvers/Queries/User.ts
@@ -1,4 +1,4 @@
-import { nonNull, queryField, stringArg } from 'nexus'
+import { list, nonNull, queryField, stringArg } from 'nexus'
 import { handleError } from '../../utils/helpers'
 import { errors } from '../../utils/constants'
 
@@ -51,6 +51,22 @@ export const exists = queryField('exists', {
       } else {
         return { exist: false, user: {email: email, id: -1} }
       }
+    } catch (e) {
+      handleError(errors.userAlreadyExists)
+    }
+  }
+})
+
+export const findUsers = queryField('findUsers', {
+  type: list('User'),
+  args: {
+    email: nonNull(stringArg())
+  },
+  async resolve(_parent, {email}, ctx){
+    try {
+      return await ctx.prisma.user.findMany({
+        select: {email}
+      })
     } catch (e) {
       handleError(errors.userAlreadyExists)
     }
